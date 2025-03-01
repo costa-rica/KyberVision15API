35c2f6628b7e06abf0a3f012fc0597aff4420bb3
Nick <nrodrig1@gmail.com>
2025-03-01 17:08:38 +0100

    refactor Match model and users routes

--- models/Match.js
+++ models/Match.js
@@ -29,9 +29,10 @@ const Match = sequelize.define(
       allowNull: true,
       field: "team_id_winner",
     },
-    GroupId: {
+    groupContractId: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      // allowNull: false,
+      field: "group_contract_id",
     },
     matchDate: {
       type: DataTypes.DATEONLY,
@@ -50,47 +51,3 @@ const Match = sequelize.define(
 );
 
 module.exports = Match;
-// // // NR: refactored 2025-02-08
-// const { DataTypes } = require("sequelize");
-// const sequelize = require("./_connection");
-
-// const Match = sequelize.define(
-//   "Match",
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       autoIncrement: true,
-//       primaryKey: true,
-//     },
-//     leagueId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     teamIdAnalyzed: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     teamIdOpponent: {
-//       type: DataTypes.INTEGER,
-//       allowNull: true,
-//     },
-//     teamIdWinner: {
-//       type: DataTypes.INTEGER,
-//       allowNull: true,
-//     },
-//     matchDate: {
-//       type: DataTypes.DATEONLY,
-//       allowNull: false,
-//     },
-//     city: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     tableName: "Match",
-//     underscored: false, // Ensures the DB columns use camelCase
-//   }
-// );
-
-// module.exports = Match;
